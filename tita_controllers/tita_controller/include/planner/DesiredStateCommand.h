4b00899560b760ce84eb44134e161859bd69364e
hilab-workshop-ldc <2482812356@qq.com>
2025-06-06 22:49:07 +0800

    初始状态改为idle，尝试

--- tita_controllers/tita_controller/include/planner/DesiredStateCommand.h
+++ tita_controllers/tita_controller/include/planner/DesiredStateCommand.h
@@ -2,7 +2,7 @@
  * @Author: hilab-workshop-ldc 2482812356@qq.com
  * @Date: 2025-03-20 15:25:00
  * @LastEditors: hilab-workshop-ldc 2482812356@qq.com
- * @LastEditTime: 2025-04-03 23:08:34
+ * @LastEditTime: 2025-06-03 16:59:21
  * @FilePath: /tita_rl_sim2sim2real/src/tita_locomotion/tita_controllers/tita_controller/include/planner/DesiredStateCommand.h
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -28,7 +28,7 @@ public:
     scalar_t two_wheel_distance;
     void zero()
     {
-      fsm_name_ = "rl";
+      fsm_name_ = "idle";
       twist_linear.Zero();
       twist_angular.Zero();
       pose_position.Zero();
